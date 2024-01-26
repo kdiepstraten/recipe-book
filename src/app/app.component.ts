8ea332ba26fb8ad213a377758350110de36260ee
kdiepstraten <kodie@live.nl>
2024-01-26 14:06:16 +0100

    Made a navigate between Recipe and Shoppinglist in the header. Now these components are seperate. Later will fix it to be more precise but for now it works. Next step is Data in recipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'recipe-book';
+  title: string = 'recipe-book';
+  loadedFeature: string = 'recipe';
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+
+
 }
--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css'
 })
-export class RecipeDetailComponent {
-
+export class RecipeDetailComponent implements OnInit{
+constructor() {
+}
+ngOnInit() {
+}
 }
