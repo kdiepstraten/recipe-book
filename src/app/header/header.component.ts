64ba944a61a9acef6f9f615fb27e9bb4d92d76b7
kdiepstraten <kodie@live.nl>
2024-01-31 09:22:58 +0100

    Made some routes. Made sure that the buttons works and the navigation too. Next is observalbes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from "@angular/core";
+import {Component, EventEmitter,  Output} from "@angular/core";
 
 @Component({
   selector: 'app-header',
@@ -8,8 +8,6 @@ export class HeaderComponent {
   collapsed = true;
   @Output() featureSelected = new EventEmitter<string>();
 
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
+
 
 }
